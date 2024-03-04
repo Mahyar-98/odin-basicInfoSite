c60552eed1659a3756c32ffd739afdfa035243f0
Mahyar <mahyar.erfanian1998@gmail.com>
2024-03-03 23:57:44 -0500

    add different html pages to the views folder and make a basic routing switch statement

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
 const http = require('http')
+const fs = require('fs')
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method)
-    res.setHeader('content-type', 'text/plain')
-    res.write('hi')
-    res.end()
+
+    let htmlPath = './views/'
+    switch (req.url) {
+        case '/':
+            htmlPath += 'index.html';
+            res.statusCode = 200;
+            break;
+        case '/about':
+            htmlPath += 'about.html';
+            res.statusCode = 200;
+            break;
+        case '/contact-me':
+            htmlPath += 'contact-me.html';
+            res.statusCode = 200;
+            break;
+        default:
+            htmlPath += '404.html'
+            res.statusCode = 404;
+            break;
+    }
+
+    res.setHeader('content-type', 'text/html')
+
+    fs.readFile(htmlPath, (err, data) => {
+        if (err) {
+            console.log(err)
+            res.end()
+        } else {
+            res.end(data)
+        }
+    })    
 })
 
 server.listen(8080, 'localhost', () => {
